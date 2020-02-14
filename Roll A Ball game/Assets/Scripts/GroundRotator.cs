edd39046e572ead46356387f81251cc03d867dbb
MichaelPloeger <m.ploeger5@hotmail.nl>
2020-02-14 15:06:19 +0100

    Moving Ground mechanic

---
+++ Roll A Ball game/Assets/Scripts/GroundRotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundRotator : MonoBehaviour
+{
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontalTilt = Input.GetAxis("Horizontal");
+        float verticalTilt = Input.GetAxis("Vertical");
+
+        Quaternion rotation = Quaternion.Euler(verticalTilt, 0.0f, -horizontalTilt);
+        rb.MoveRotation(rb.rotation * rotation);
+    }
+}
