6580c99690d82de670e63166c90c782abb2c57ce
Sjoerd van Swetselaar <MS.vanSwetselaar@student.han.nl>
2020-02-14 15:17:31 +0100

    playercontroller en kleuren ding

---
+++ Roll A Ball game/Assets/Scripts/ColourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ColourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeMaterial(GetComponent<Renderer>());
+    }
+    
+    void ChangeMaterial(Renderer pickUpRenderer)
+    {
+        Color color = Color.red;
+        float colourRandom = Random.Range(1, 4);
+        print(colourRandom);
+        switch(colourRandom)
+        {
+            case 1:
+                color = Color.red;
+                break;
+            case 2:
+                color = Color.green;
+                break;
+            case 3:
+                color = Color.blue;
+                break;
+        }
+        pickUpRenderer.material.color = color;
+    }
+}
--- Roll A Ball game/Assets/Scripts/PlayerController.cs
+++ Roll A Ball game/Assets/Scripts/PlayerController.cs
@@ -1,52 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using System.Diagnostics;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour {
-
-    public float speed;
-    public Text countText;
-    public Text winText;
-
-    private Rigidbody rb;
-    private int count;
-
-    void Start ()
-    {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText ();
-        winText.text = "";
-    }
-
-    void FixedUpdate ()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
-
-    void OnTriggerEnter(Collider other) 
-    {
-        if (other.gameObject.CompareTag ( "Pick Up"))
-        {
-            other.gameObject.SetActive (false);
-            count = count + 1;
-            SetCountText ();
-        }
-    }
-
-    void SetCountText ()
-    {
-        countText.text = "Count: " + count.ToString ();
-        if (count >= 8)
-        {
-            winText.text = "You Win!";
-        }
-    }
+	
+	public float speed;
+	
+	private Rigidbody rb;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+   
+   void FixedUpdate () {
+	   float moveHorizontal = Input.GetAxis("Horizontal");
+	   float moveVertical = Input.GetAxis("Vertical");
+	   
+	   Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+	   
+	   rb.AddForce(movement * speed);
+   }
+   
+   void OnTriggerEnter(Collider other) {
+	   if(other.gameObject.CompareTag("Pick Up")) {
+		   other.gameObject.SetActive(false);
+		   switch (ColorUtility.ToHtmlStringRGB(other.gameObject.GetComponent<Renderer>().material.color))
+		   {
+			   case "0000FF":
+				   speed *= 0.5f;
+				   break;
+			   case "00FF00":
+				   Vector3 force = Vector3.up * 10;
+				   GetComponent<Transform>().position += force;
+				   break;
+			   case "FF0000":
+				   speed *= 1.5f;
+				   break;
+		   }
+	   }
+   }
 }
